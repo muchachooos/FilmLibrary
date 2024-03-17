678212f054a13c30e52453e2a4164ebe577f8549
muchachooos <odnorogg@bk.ru>
2024-03-18 00:34:09 +0300

    Modify model

--- model/model.go
+++ model/model.go
@@ -18,15 +18,9 @@ type DBConf struct {
 	Sslmode  string `json:"sslmode"`
 }
 
-type ActorJSON struct {
-	Name   string    `json:"actor_name"`
-	Gender string    `json:"gender"`
-	Birth  time.Time `json:"date_of_birth"`
-}
-
 type Actor struct {
-	ID     string    `db:"id"`
-	Name   string    `db:"actor_name"`
-	Gender string    `db:"gender"`
-	Birth  time.Time `db:"date_of_birth"`
+	ID     string    `db:"id" json:"id"`
+	Name   string    `db:"name" json:"name"`
+	Gender string    `db:"gender" json:"gender"`
+	Birth  time.Time `db:"date_of_birth" json:"date_of_birth"`
 }
