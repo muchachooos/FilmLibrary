9bd8af9547b2f97f359c37cab69dbb37d76ddc34
muchachooos <odnorogg@bk.ru>
2024-03-18 02:34:48 +0300

    Modify actor handler

--- handler/actor.go
+++ handler/actor.go
@@ -1,10 +1,11 @@
 package handler
 
 import (
-	"FilmLibrary/helpers/id"
+	"FilmLibrary/helpers/idgen"
 	"FilmLibrary/helpers/parser"
 	"FilmLibrary/model"
 	"encoding/json"
+	"errors"
 	"net/http"
 )
 
@@ -21,12 +22,12 @@ func (s *Server) ActorsHandler(res http.ResponseWriter, req *http.Request) {
 
 		if actorReq.Name == "" {
 			res.WriteHeader(http.StatusBadRequest)
-			res.Write([]byte("No name"))
+			errors.New("no name")
 			return
 		}
 
 		actor := model.Actor{
-			ID:     id.NewID(),
+			ID:     idgen.NewID(),
 			Name:   actorReq.Name,
 			Gender: actorReq.Gender,
 			Birth:  actorReq.Birth,
@@ -35,38 +36,11 @@ func (s *Server) ActorsHandler(res http.ResponseWriter, req *http.Request) {
 		err = s.Storage.CreateActor(actor)
 		if err != nil {
 			res.WriteHeader(http.StatusInternalServerError)
-			res.Write([]byte("Error: Create Actor error"))
+			errors.New("create Actor error")
 			return
 		}
 
 		res.WriteHeader(http.StatusOK)
-		return
-
-	case http.MethodGet:
-		actorId := req.FormValue("id")
-		if actorId == "" {
-			res.WriteHeader(http.StatusBadRequest)
-			res.Write([]byte("No actor ID"))
-			return
-		}
-
-		actor, err := s.Storage.GetActor(actorId)
-		if err != nil {
-			res.WriteHeader(http.StatusInternalServerError)
-			res.Write([]byte("Error: Get Actor error"))
-			return
-		}
-
-		response, err := json.Marshal(actor)
-		if err != nil {
-			res.WriteHeader(http.StatusInternalServerError)
-			res.Write([]byte("Error: Marshal error"))
-			return
-		}
-
-		res.WriteHeader(http.StatusOK)
-		res.Write(response)
-
 		return
 
 	case http.MethodPut:
@@ -79,7 +53,7 @@ func (s *Server) ActorsHandler(res http.ResponseWriter, req *http.Request) {
 
 		if actorReq.Name == "" {
 			res.WriteHeader(http.StatusBadRequest)
-			res.Write([]byte("No name"))
+			errors.New("no name")
 			return
 		}
 
@@ -100,16 +74,84 @@ func (s *Server) ActorsHandler(res http.ResponseWriter, req *http.Request) {
 		response, err := json.Marshal(actor)
 		if err != nil {
 			res.WriteHeader(http.StatusInternalServerError)
-			res.Write([]byte("Error: Marshal error"))
+			errors.New("marshal error")
 			return
 		}
 
 		res.WriteHeader(http.StatusOK)
 		res.Write(response)
-
 		return
 
 	case http.MethodDelete:
+		actorId := req.FormValue("idgen")
+		if actorId == "" {
+			res.WriteHeader(http.StatusBadRequest)
+			errors.New("no actor ID")
+			return
+		}
 
+		err := s.Storage.DeleteActor(actorId)
+		if err != nil {
+			res.WriteHeader(http.StatusInternalServerError)
+			errors.New("error: Get Actor error")
+			return
+		}
+
+		res.WriteHeader(http.StatusOK)
+		return
+	}
+}
+
+func (s *Server) CastHandler(res http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodPost:
+		actorId := req.FormValue("actor_id")
+		if actorId == "" {
+			res.WriteHeader(http.StatusBadRequest)
+			errors.New("no actor ID")
+			return
+		}
+
+		movieId := req.FormValue("movie_id")
+		if movieId == "" {
+			res.WriteHeader(http.StatusBadRequest)
+			errors.New("no movie ID")
+			return
+		}
+
+		err := s.Storage.AddInCast(actorId, movieId)
+		if err != nil {
+			res.WriteHeader(http.StatusInternalServerError)
+			errors.New("added to the cast error")
+			return
+		}
+
+		res.WriteHeader(http.StatusOK)
+		return
+
+	case http.MethodDelete:
+		actorId := req.FormValue("actor_id")
+		if actorId == "" {
+			res.WriteHeader(http.StatusBadRequest)
+			errors.New("no actor ID")
+			return
+		}
+
+		movieId := req.FormValue("movie_id")
+		if movieId == "" {
+			res.WriteHeader(http.StatusBadRequest)
+			errors.New("no movie ID")
+			return
+		}
+
+		err := s.Storage.DeleteFromCast(actorId, movieId)
+		if err != nil {
+			res.WriteHeader(http.StatusInternalServerError)
+			errors.New("delete cast error")
+			return
+		}
+
+		res.WriteHeader(http.StatusOK)
+		return
 	}
 }
