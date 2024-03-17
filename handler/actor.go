b058495022836b79da62f19e92a14c203a07372f
muchachooos <odnorogg@bk.ru>
2024-03-18 00:33:37 +0300

    Modify actor handler

--- handler/actor.go
+++ handler/actor.go
@@ -4,7 +4,7 @@ import (
 	"FilmLibrary/helpers/id"
 	"FilmLibrary/helpers/parser"
 	"FilmLibrary/model"
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
@@ -12,7 +12,7 @@ func (s *Server) ActorsHandler(res http.ResponseWriter, req *http.Request) {
 	switch req.Method {
 	case http.MethodPost:
 
-		var actorReq model.ActorJSON
+		var actorReq model.Actor
 		err := parser.ParseBody(req, &actorReq)
 		if err != nil {
 			res.WriteHeader(http.StatusBadRequest)
@@ -32,10 +32,10 @@ func (s *Server) ActorsHandler(res http.ResponseWriter, req *http.Request) {
 			Birth:  actorReq.Birth,
 		}
 
-		err = s.Storage.ActorsHandlerInDB(actor)
+		err = s.Storage.CreateActor(actor)
 		if err != nil {
 			res.WriteHeader(http.StatusInternalServerError)
-			res.Write([]byte("Error: Database error"))
+			res.Write([]byte("Error: Create Actor error"))
 			return
 		}
 
@@ -43,16 +43,73 @@ func (s *Server) ActorsHandler(res http.ResponseWriter, req *http.Request) {
 		return
 
 	case http.MethodGet:
-		id := req.FormValue("id")
-		if id == "" {
+		actorId := req.FormValue("id")
+		if actorId == "" {
 			res.WriteHeader(http.StatusBadRequest)
 			res.Write([]byte("No actor ID"))
 			return
 		}
 
-		fmt.Println(id)
+		actor, err := s.Storage.GetActor(actorId)
+		if err != nil {
+			res.WriteHeader(http.StatusInternalServerError)
+			res.Write([]byte("Error: Get Actor error"))
+			return
+		}
+
+		response, err := json.Marshal(actor)
+		if err != nil {
+			res.WriteHeader(http.StatusInternalServerError)
+			res.Write([]byte("Error: Marshal error"))
+			return
+		}
 
 		res.WriteHeader(http.StatusOK)
+		res.Write(response)
+
 		return
+
+	case http.MethodPut:
+		var actorReq model.Actor
+		err := parser.ParseBody(req, &actorReq)
+		if err != nil {
+			res.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		if actorReq.Name == "" {
+			res.WriteHeader(http.StatusBadRequest)
+			res.Write([]byte("No name"))
+			return
+		}
+
+		actor := model.Actor{
+			ID:     actorReq.ID,
+			Name:   actorReq.Name,
+			Gender: actorReq.Gender,
+			Birth:  actorReq.Birth,
+		}
+
+		actor, err = s.Storage.UpdateActor(actor)
+		if err != nil {
+			res.WriteHeader(http.StatusInternalServerError)
+			res.Write([]byte(err.Error()))
+			return
+		}
+
+		response, err := json.Marshal(actor)
+		if err != nil {
+			res.WriteHeader(http.StatusInternalServerError)
+			res.Write([]byte("Error: Marshal error"))
+			return
+		}
+
+		res.WriteHeader(http.StatusOK)
+		res.Write(response)
+
+		return
+
+	case http.MethodDelete:
+
 	}
 }
