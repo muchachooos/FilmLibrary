c5075206c9faeb56d3eb055b0b172c1edabbd52c
muchachooos <odnorogg@bk.ru>
2024-03-18 00:33:48 +0300

    Modify actor storage

--- storage/actor.go
+++ storage/actor.go
@@ -2,10 +2,12 @@ package storage
 
 import (
 	"FilmLibrary/model"
+	"errors"
+	"fmt"
 )
 
-func (s *Storage) ActorsHandlerInDB(actor model.Actor) error {
-	result, err := s.DB.Exec("INSERT INTO actors (id, actor_name,  gender, date_of_birth) VALUES ($1, $2, $3, $4)", actor.ID, actor.Name, actor.Gender, actor.Birth)
+func (s *Storage) CreateActor(actor model.Actor) error {
+	result, err := s.DB.Exec("INSERT INTO actor (id, name,  gender, date_of_birth) VALUES ($1, $2, $3, $4)", actor.ID, actor.Name, actor.Gender, actor.Birth)
 	if err != nil {
 		return err
 	}
@@ -20,4 +22,44 @@ func (s *Storage) ActorsHandlerInDB(actor model.Actor) error {
 	}
 
 	return nil
+}
+
+func (s *Storage) GetActor(actorId string) (model.Actor, error) {
+	var actor model.Actor
+
+	err := s.DB.Get(&actor, "SELECT * FROM actor WHERE id = $1", actorId)
+	if err != nil {
+		return model.Actor{}, err
+	}
+
+	return actor, nil
+}
+
+func (s *Storage) UpdateActor(actor model.Actor) (model.Actor, error) {
+	result, err := s.DB.NamedExec("UPDATE actor SET name = :name, gender = :gender, date_of_birth = :date_of_birth WHERE id = :id", actor)
+	if err != nil {
+		fmt.Println(111111111111)
+		return model.Actor{}, err
+	}
+
+	countOfModifiedRows, err := result.RowsAffected()
+	if err != nil {
+		fmt.Println(222222222222)
+		return model.Actor{}, err
+	}
+
+	if countOfModifiedRows == 0 {
+		fmt.Println(33333333333)
+		return model.Actor{}, errors.New("Nothing has been changed")
+	}
+
+	var response model.Actor
+
+	err = s.DB.Get(&response, "SELECT * FROM actor WHERE id = $1", actor.ID)
+	if err != nil {
+		fmt.Println(44444444444444)
+		return model.Actor{}, err
+	}
+
+	return actor, nil
 }
