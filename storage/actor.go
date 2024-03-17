0ad0f56e6aeb19989dff195de3476b38616db736
muchachooos <odnorogg@bk.ru>
2024-03-18 02:35:00 +0300

    Modify actor storage

--- storage/actor.go
+++ storage/actor.go
@@ -3,11 +3,10 @@ package storage
 import (
 	"FilmLibrary/model"
 	"errors"
-	"fmt"
 )
 
 func (s *Storage) CreateActor(actor model.Actor) error {
-	result, err := s.DB.Exec("INSERT INTO actor (id, name,  gender, date_of_birth) VALUES ($1, $2, $3, $4)", actor.ID, actor.Name, actor.Gender, actor.Birth)
+	result, err := s.DB.NamedExec(`INSERT INTO actor (idgen, name,  gender, date_of_birth) VALUES (:idgen, :name, :gender, :date_of_birt, :date_of_birth)`, actor)
 	if err != nil {
 		return err
 	}
@@ -24,10 +23,24 @@ func (s *Storage) CreateActor(actor model.Actor) error {
 	return nil
 }
 
-func (s *Storage) GetActor(actorId string) (model.Actor, error) {
-	var actor model.Actor
+func (s *Storage) UpdateActor(actor model.Actor) (model.Actor, error) {
+	result, err := s.DB.NamedExec(`UPDATE actor SET name = :name, gender = :gender,date_of_birth = :date_of_birth WHERE idgen = :idgen`, actor)
+	if err != nil {
+		return model.Actor{}, err
+	}
 
-	err := s.DB.Get(&actor, "SELECT * FROM actor WHERE id = $1", actorId)
+	countOfModifiedRows, err := result.RowsAffected()
+	if err != nil {
+		return model.Actor{}, err
+	}
+
+	if countOfModifiedRows == 0 {
+		return model.Actor{}, errors.New("Nothing has been changed")
+	}
+
+	var response model.Actor
+
+	err = s.DB.Get(&response, "SELECT * FROM actor WHERE idgen = $1", actor.ID)
 	if err != nil {
 		return model.Actor{}, err
 	}
@@ -35,31 +48,38 @@ func (s *Storage) GetActor(actorId string) (model.Actor, error) {
 	return actor, nil
 }
 
-func (s *Storage) UpdateActor(actor model.Actor) (model.Actor, error) {
-	result, err := s.DB.NamedExec("UPDATE actor SET name = :name, gender = :gender, date_of_birth = :date_of_birth WHERE id = :id", actor)
-	if err != nil {
-		fmt.Println(111111111111)
-		return model.Actor{}, err
-	}
+func (s *Storage) DeleteActor(id string) error {
+	result, err := s.DB.Exec(`DELETE FROM actor WHERE idgen = $1`, id)
 
 	countOfModifiedRows, err := result.RowsAffected()
 	if err != nil {
-		fmt.Println(222222222222)
-		return model.Actor{}, err
+		return err
 	}
 
 	if countOfModifiedRows == 0 {
-		fmt.Println(33333333333)
-		return model.Actor{}, errors.New("Nothing has been changed")
+		errors.New("Nothing has been changed")
 	}
 
-	var response model.Actor
+	return nil
+}
 
-	err = s.DB.Get(&response, "SELECT * FROM actor WHERE id = $1", actor.ID)
+func (s *Storage) AddInCast(actorID, movieID string) error {
+	_, err := s.DB.Exec(`INSERT INTO cast_record (actor_id, movie_id) VALUES ($1, $2)`, actorID, movieID)
+
+	return err
+}
+
+func (s *Storage) DeleteFromCast(actorID, movieID string) error {
+	result, err := s.DB.Exec(`DELETE FROM actor WHERE actor_id = $1 AND movie_id= $2`, actorID, movieID)
+
+	countOfModifiedRows, err := result.RowsAffected()
 	if err != nil {
-		fmt.Println(44444444444444)
-		return model.Actor{}, err
+		return err
 	}
 
-	return actor, nil
+	if countOfModifiedRows == 0 {
+		errors.New("Nothing has been changed")
+	}
+
+	return err
 }
